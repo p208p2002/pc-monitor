d43c4670656069077746edaf4a044c2d280b1f09
Philip <p208p2002@gmail.com>
2020-08-25 14:04:02 +0800

    change style

--- src/App.js
+++ src/App.js
@@ -7,6 +7,31 @@ if (!cpu.isSupported()) {
   throw new Error('windows-cpu is not supported on this platform');
 }
 
+function StatsBar(props) {
+  let { rate, color = 'red' } = props
+  let green =  255 - Math.round(2.55*rate)
+  let red = Math.round(2.55*rate)
+  return (
+    <div style={{ position: 'relative', top: 0 }}>
+      <span style={{
+        width: `${rate}%`,
+        height: '20px',
+        display: 'inline-block',
+        backgroundColor: color === 'auto'?`rgb(${red},${green},0)`:color,
+        transition: 'all 1s',
+        opacity: 0.5,
+      }} />
+      <div style={{
+        position: 'absolute',
+        top: 1,
+        left: 5
+      }}>
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -15,7 +40,7 @@ class App extends Component {
       cpuLoad: 0,
       freeMem: 0,
       usedMem: 0,
-      totalMem: Math.round(os.totalmem()/(2**20)),
+      totalMem: Math.round(os.totalmem() / (2 ** 20)),
       osType: os.type(),
     }
 
@@ -26,32 +51,38 @@ class App extends Component {
 
     setInterval(() => {
       cpu.totalLoad().then(load => {
-        let freemem = os.freemem()/2**20
+        let freemem = os.freemem() / 2 ** 20
         let { totalMem } = this.state
-        
+
         this.setState({
           cpuLoad: load,
           freeMem: freemem,
           usedMem: Math.round(totalMem - freemem)
         })
       });
-    }, 500)
+    }, 1000)
   }
 
   render() {
     let { cpus, cpuLoad, totalMem, osType, usedMem } = this.state;
+    let memLoad = Math.round(usedMem / totalMem * 100)
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-12">
-            <span>OS&nbsp;:&nbsp;{osType}</span><br />
-            {/* {cpus.map((cpu,i)=>{
-              return(
-                <><span className="">CPU{`${i}`}&nbsp;:&nbsp;{cpu.model}&nbsp;({cpuLoad}%)</span><br /></>
-              )
-            })} */}
-            <span className="">CPU&nbsp;:&nbsp;{cpus[0].model}&nbsp;({cpuLoad}%)</span><br />
-            <span>MEM&nbsp;:&nbsp;{usedMem}&nbsp;/&nbsp;{totalMem}&nbsp;MB&nbsp;({Math.round(usedMem/totalMem*100)}%)</span><br />
+            <small style={{
+              position: "absolute",
+              marginRight: 5,
+              right: 18,
+              bottom: 5
+            }}>{osType}</small><br />
+            <StatsBar rate={cpuLoad} color='auto'>
+              <span>CPU&nbsp;:&nbsp;{cpus[0].model}&nbsp;({cpuLoad}%)</span><br />
+            </StatsBar>
+            <StatsBar rate={memLoad} color='auto'>
+              <span>MEM&nbsp;:&nbsp;{usedMem}&nbsp;/&nbsp;{totalMem}&nbsp;MB&nbsp;({memLoad}%)</span><br />
+            </StatsBar>
+            {/* <StatsBar rate={memLoad} /> */}
           </div>
         </div>
       </div>
